74bdf451bf8feb6b7b63e7b0d839ae4bd8fad0e4
Gerber Neves <139247388+Gerbercode@users.noreply.github.com>
2025-01-08 14:09:13 +0100

    Subindo os arquivos do projeto
    Fiz o upload de todos os arquivos do projeto...

---
+++ index.js
@@ -0,0 +1,71 @@
+const button = document.querySelector(".button-task");
+const input = document.querySelector(".input-task");
+const listaCompleta = document.querySelector(".list-task");
+
+let minhaListaDeItens = [];
+
+function adicionarTarefa() {
+  minhaListaDeItens.push({
+    tarefa: input.value,
+    concluida: false,
+
+
+
+
+  });
+
+  input.value = "";
+  mostrarTarefa();
+}
+
+function mostrarTarefa() {
+  let novaLista = "";
+
+  minhaListaDeItens.forEach((item, posicao) => {
+    novaLista =
+      novaLista +
+      `       
+        <li class="task ${item.concluida && "done"} ">
+        <img title="Concluir tarefa" src="./img/Checkmark.png" alt="concluido" onclick="concluirTarefa(${posicao})"> 
+        <p>${item.tarefa}</p>
+            
+        <img title="Excluir tarefa" src="./img/Cancel.png" alt="excluir"  onclick = "deletarItem(${posicao})">
+        </li> `;
+  });
+
+  listaCompleta.innerHTML = novaLista;
+
+  localStorage.setItem("lista", JSON.stringify(minhaListaDeItens));
+}
+
+function concluirTarefa(posicao) {
+  minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida;
+
+  mostrarTarefa();
+}
+
+function deletarItem(posicao) {
+  minhaListaDeItens.splice(posicao, 1);
+
+  mostrarTarefa();
+}
+
+function recarregarTarefa() {
+  const tarefaLocalStorage = localStorage.getItem("lista");
+
+  if (tarefaLocalStorage) {
+    minhaListaDeItens = JSON.parse(tarefaLocalStorage);
+    console.log(tarefaLocalStorage);
+  }
+
+  mostrarTarefa();
+}
+recarregarTarefa();
+button.addEventListener("click", adicionarTarefa);
+
+
+
+
+//let time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
+//if (time.includes("Hour") || time.includes("Minute") || time.includes("AM/PM")) {
+    //return alert("Please, select a valid time to set Alarm!");
